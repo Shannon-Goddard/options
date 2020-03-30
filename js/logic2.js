abb319694262ea2ae3c2b00ac1e716b8afc071d4
Shannon Goddard <Uthingkithunk@aol.com>
2020-03-29 23:49:42 -0700

    adding code for second table-not working

---
+++ js/logic2.js
@@ -0,0 +1,24 @@
+google.load('visualization', '1', {
+    packages: ['table']
+});
+var visualization;
+
+function drawVisualization() {
+    var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1fgQa_P4NNBpaSmj0A765siDunzQkZL5Pvo2LXnR4_58/edit?usp=sharing');
+    query.setQuery('SELECT B, C, D, E, K, M, N, F label C "call / put"');
+    query.send(handleQueryResponse);
+}
+
+function handleQueryResponse(response) {
+    if (response.isError()) {
+        alert('There was a problem with your query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+    var data = response.getDataTable();
+    visualization = new google.visualization.Table(document.getElementById('table'));
+    visualization.draw(data, {
+        allowHtml: true,
+        legend: 'bottom'
+    });
+}
+google.setOnLoadCallback(drawVisualization);
